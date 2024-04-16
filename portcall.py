8cc89754628308e93a333210d68331edab72e6c4
Razib Hasan <razib.hasan@tuni.fi>
2024-04-17 02:28:46 +0300

    dockerfile

--- portcall.py
+++ portcall.py
@@ -1,1 +1,10 @@
-print("There are 42 portcalls")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "There are 42 portcalls"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
