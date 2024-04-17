1f47f5c4376a534dea1f317e224a62941e50e4d2
Razib Hasan <razib.hasan@tuni.fi>
2024-04-17 14:05:59 +0300

    just playing aroundd

--- portcall.py
+++ portcall.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "There are 42 portcalls\nMore ships on their way."
+    return "There are 42 portcalls exercise is done now"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
